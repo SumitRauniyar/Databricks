690de6f01dec7483c65d7f6f114cc6b383dacc8d
Sumit Rauniyar <rauniyar555@gmail.com>
2019-01-26 21:45:01 +0530

    added

--- config/vars.py
+++ config/vars.py
@@ -1,1 +1,3 @@
 INPUT_FILE="/Users/sumitrauniyar/Documents/gcp_environment/crime.csv"
+PROJECT_ID='edm-shared-development'
+STAGING_DATASET='RSCH_NLSN_NTL_STG_TEST_1'
--- python/createtables.py
+++ python/createtables.py
@@ -1,7 +1,7 @@
 from google.cloud import bigquery
-
 from config import vars
-from config import schema
+from config import schema_fike
+
 class CreateTables:
 
     def __init__(self):
@@ -9,15 +9,22 @@ class CreateTables:
         self.staging_dataset=vars.STAGING_DATASET
 
     def createtable(self):
-        client = bigquery.client(self.project_id)
+        client = bigquery.Client(self.project_id)
         dataset_ref = client.dataset(self.staging_dataset)
         table_ref = dataset_ref.table('CRIME_RATE')
         schema=[]
-        for column in schema.schema:
+        for column in schema_fike.schema:
             name=column["name"]
             type=column["type"]
             required=column["required"]
             schema.append(bigquery.SchemaField(name,type,required))
 
         table = bigquery.Table(table_ref, schema=schema)
-        client.create_table(table)
+        if client.get_table(table_ref=table_ref):
+            print("Table already exists")
+        else:
+            client.create_table(table)
+
+
+createtablesobj=CreateTables()
+createtablesobj.createtable()
